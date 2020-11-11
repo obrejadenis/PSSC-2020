c54a348ea3273b02aec120fcd3809b18c0f9189e
Denis Obreja <obrejadenis@gmail.com>
2020-11-11 20:24:15 +0200

    tema6

---
+++ ObrejaDenis/L06/Samples/StackUnderflow.API.Rest/Controllers/QuestionsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Access.Primitives.Extensions.ObjectExtensions;
+using Access.Primitives.IO;
+using Microsoft.AspNetCore.Mvc;
+using StackUnderflow.Backend.Interfaces.Responses;
+using StackUnderflow.Domain.Core;
+using StackUnderflow.Domain.Core.Contexts;
+using StackUnderflow.Domain.Schema.Backoffice.CreateTenantOp;
+using StackUnderflow.EF.Models;
+using Access.Primitives.EFCore;
+using StackUnderflow.Domain.Core.Contexts.Questions;
+using StackUnderflow.Domain.Schema.Questions.CreateAnswerOp;
+using static StackUnderflow.Domain.Schema.Questions.CreateAnswerOp.CreateReplyResult;
+
+namespace StackUnderflow.API.Rest.Controllers
+{
+    [ApiController]
+    [Route("backoffice")]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IInterpreterAsync _interpreter;
+        private readonly StackUnderflowContext _dbContext;
+
+        public QuestionsController(IInterpreterAsync interpreter, StackUnderflowContext dbContext)
+        {
+            _interpreter = interpreter;
+            _dbContext = dbContext;
+        }
+
+        [HttpPost("createReply")]
+        public async Task<IActionResult> CreateReplyAndAcknowledgement([FromBody] CreateReplyCmd createReplyCmd)
+        {
+           
+            
+            var expr = from createReplyResult in QuestionsDomain.CreateReply(createReplyCmd.QuestionId, createReplyCmd.AuthorUserId,createReplyCmd.QuestionOwnerId, createReplyCmd.Body)
+                       let reply = createReplyResult.SafeCast<ReplyCreated>().Select(r => r)
+                       from checkLanguageResult in QuestionsDomain.CheckLanguage(createReplyCmd.Body)
+                       from sendQuestionOwnerAcknowledgementResult in QuestionsDomain.SendQuestionOwnerAcknowledgement(createReplyCmd.QuestionId, createReplyCmd.QuestionOwnerId)
+                       from sendReplyAuthorAcknowledgementResult in QuestionsDomain.SendReplyAuthorAcknowledgement(createReplyCmd.AuthorUserId, createReplyCmd.QuestionOwnerId, reply)
+                       select new { createReplyResult, checkLanguageResult, sendQuestionOwnerAcknowledgementResult, sendReplyAuthorAcknowledgementResult };
+
+            var ctx = new QuestionReadContext(new List<Post>());
+            var r = await _interpreter.Interpret(expr, ctx);
+
+            await _dbContext.SaveChangesAsync();
+
+            return r.createReplyResult.Match(
+                created => (IActionResult)Ok("Reply added"),
+                notCreated => BadRequest("Reply not added"),
+                whenInvalidRequest => BadRequest("Invalid request"));
+        }
+
+
+
+    }
+}
---
+++ ObrejaDenis/L06/Samples/StackUnderflow.Backoffice.Adapters/CreateReply/CreateReplyAdaptor.cs
@@ -0,0 +1,30 @@
+﻿using Access.Primitives.Extensions.ObjectExtensions;
+using Access.Primitives.IO;
+using StackUnderflow.Domain.Core.Contexts;
+using StackUnderflow.Domain.Schema.Questions.CreateAnswerOp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static StackUnderflow.Domain.Schema.Questions.CreateAnswerOp.CreateReplyResult;
+
+namespace StackUnderflow.Adapters.CreateReply
+{
+    public partial class CreateReplyAdaptor : Adapter<CreateReplyCmd, ICreateReplyResult, QuestionsReadContext >
+    {
+        public override Task PostConditions(CreateReplyCmd cmd, ICreateReplyResult result, QuestionsReadContext state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async override Task<ICreateReplyResult> Work(CreateReplyCmd cmd, QuestionsReadContext state)
+        {
+            var result = from validate in cmd.TryValidate()
+                         select validate;
+            return await result.Match(
+                Succ: valid => (ICreateReplyResult)(new ReplyCreated(1, cmd.QuestionId, cmd.AuthorUserId, cmd.Body)),
+                Fail: ex => (ICreateReplyResult)(new ReplyNotCreated(ex.Message))
+                );
+        }
+    }
+}
